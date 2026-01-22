297a87b9ee4f74a0191b0772f5e07bdede779afe
aneeb02 <aneeb.02@gmail.com>
2026-01-22 23:02:43 +0500

    fix: wss issue

--- pojek/settings.py
+++ pojek/settings.py
@@ -157,7 +157,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # for production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
